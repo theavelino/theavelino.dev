a1292ddab1ff17de7302d618e03ed546fac3ddc5
devmatheusss <matheusma1325@gmail.com>
2025-03-16 19:23:55 -0400

    feat: added projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,106 @@
 import { ArrowLeftIcon } from "lucide-react";
 import { Link } from "next-view-transitions";
+import Image from "next/image";
+
+import Portfolio from '@/assets/portfolio.png'
+import FinanceAIImage from '@/assets/finance_ai.png'
+
+const projects = [
+  {
+    id: 1,
+    name: "Portfolio",
+    image: Portfolio,
+    repository: "https:/github.com/devmatheusss/theavelino.dev",
+    technologies: [
+      {
+        name: "NextJS",
+        colors: {
+          background: "rgb(38 38 38 / 0.5)",
+          text: "rgb(163 163 163)"
+        },
+      },
+      {
+        name: "TypeScript",
+        colors: {
+          background: "rgb(30 64 175 / 0.5)",
+          text: "rgb(96 165 250)"
+        },
+      },
+      {
+        name: "TailwindCSS",
+        colors: {
+          background: "rgb(21 94 117 / 0.5)",
+          text: "rgb(34 211 238)"
+        },
+      },
+    ]
+  },
+  {
+    id: 2,
+    name: "finance.ai",
+    image: FinanceAIImage,
+    repository: "https:/github.com/devmatheusss/finance.ai",
+    technologies: [
+      {
+        name: "NextJS",
+        colors: {
+          background: "rgb(38 38 38 / 0.5)",
+          text: "rgb(163 163 163)"
+        },
+      },
+      {
+        name: "TypeScript",
+        colors: {
+          background: "rgb(30 64 175 / 0.5)",
+          text: "rgb(96 165 250)"
+        },
+      },
+      {
+        name: "TailwindCSS",
+        colors: {
+          background: "rgb(21 94 117 / 0.5)",
+          text: "rgb(34 211 238)"
+        },
+      },
+    ]
+  },
+]
 
 export default function ProjectsPage() {
   return (
-    <div className="col-span-2 rounded-xl bg-blue-600/20 shadow-xl p-4 flex flex-col gap-4">
+    <div className="flex-1 rounded-xl flex flex-col gap-4">
       <div className="flex items-center gap-2">
-        <Link href="/" title="Back to about me" className="opacity-50 hover:opacity-100 transition-opacity" >
+        <Link href="/" title="Back to about me" className="opacity-20 hover:opacity-50 transition-opacity" >
           <ArrowLeftIcon size={28} fontWeight="700" />
         </Link>
         <h1 className="text-3xl font-semibold">Projects</h1>
       </div>
+
+      <section className="grid grid-cols-2 gap-4">
+        {projects.map(project => (
+          <Link href={project.repository} target="_blank" rel="noreferrer external" key={project.id} className="flex flex-col justify-between w-full relative overflow-hidden p-4 aspect-video rounded-lg hover:scale-105 transition-all ease-in-out border-2 border-transparent hover:border-neutral-700 group">
+            <Image
+              src={project.image}
+              alt={project.name}
+              width={1920}
+              height={1080}
+              className="absolute aspect-video object-cover inset-0 -z-10 opacity-50 group-hover:opacity-100 transition"
+            />
+            <div className="flex items-center gap-3">
+              <h3 className="text-lg font-medium">{project.name}</h3>
+              <span className="text-sm font-medium text-neutral-400">2 weeks ago</span>
+            </div>
+            <ul className="flex gap-2 text-xs">
+              {project.technologies.map(tech => (
+                <li key={tech.name} className="px-2 py-1 rounded-full" style={{ backgroundColor: tech.colors.background, color: tech.colors.text }}>{tech.name}</li>
+              ))}
+            </ul>
+          </Link>
+        ))}
+        <div className="flex items-center justify-center bg-neutral-800/50 backdrop-blur-sm select-none aspect-video rounded-lg transition-all ease-in-out border-2 border-transparent hover:border-neutral-700 group">
+          <span>Coming Soon...</span>
+        </div>
+      </section>
     </div>
   )
 }
--- src/components/custom-link.tsx
+++ src/components/custom-link.tsx
@@ -8,11 +8,11 @@ interface CustomLinkProps extends LinkProps {
 export function CustomLink({ children, overlayLabel, ...props }: CustomLinkProps) {
   return (
     <div className='relative'>
-      <Link {...props} target='_blank' rel='external noreferrer' className="peer rounded-lg transition aspect-square w-full flex items-center justify-center hover:bg-white/20">
+      <Link {...props} target='_blank' rel='external noreferrer' className="p-3 sm:p-0 peer group rounded-base transition aspect-square w-full flex items-center justify-center">
         {children}
         <span className='sr-only'>{overlayLabel}</span>
       </Link>
-      <span className="block opacity-0 peer-hover:opacity-100 select-none absolute right-full top-1/2 -translate-y-1/2 px-2 py-1 rounded-lg text-xs bg-neutral-800/80 mr-3 transition">
+      <span className="hidden sm:block opacity-0 peer-hover:opacity-100 select-none absolute right-full top-1/2 -translate-y-1/2 px-2 py-1 rounded-base text-xs bg-neutral-800/80 mr-3 transition">
         {overlayLabel}
       </span>
     </div>
