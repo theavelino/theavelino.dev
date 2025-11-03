2c9cb56cbff4810efd715fa3647e7659f6891c3f
theavelino <matheusma1325@gmail.com>
2025-11-03 19:59:42 -0400

    fix: external link

--- src/components/custom-link.tsx
+++ src/components/custom-link.tsx
@@ -1,6 +1,7 @@
 import Link, { type LinkProps } from 'next/link'
+import { AnchorHTMLAttributes } from 'react'
 
-interface CustomLinkProps extends LinkProps {
+interface CustomLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   children: React.ReactNode,
   overlayLabel: string
 }
@@ -8,10 +9,10 @@ interface CustomLinkProps extends LinkProps {
 export function CustomLink({ children, overlayLabel, ...props }: CustomLinkProps) {
   return (
     <div className='relative'>
-      <Link {...props} target='_blank' rel='external noreferrer' className="p-3 sm:p-0 peer group rounded-base transition aspect-square w-full flex items-center justify-center">
+      <a {...props} target='_blank' rel='external noreferrer' className="p-3 sm:p-0 peer group rounded-base transition aspect-square w-full flex items-center justify-center">
         {children}
         <span className='sr-only'>{overlayLabel}</span>
-      </Link>
+      </a>
       <span className="hidden sm:block opacity-0 peer-hover:opacity-100 select-none absolute right-full top-1/2 -translate-y-1/2 px-2 py-1 rounded-base text-xs bg-neutral-800/80 mr-3 transition">
         {overlayLabel}
       </span>
